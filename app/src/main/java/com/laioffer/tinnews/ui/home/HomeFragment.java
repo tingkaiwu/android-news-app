a6f91b6c48a24d3000c76871bc743661ec3a39b3
Ting Kai Wu <tingkaiwuu@gmail.com>
2020-06-07 00:22:32 -0400

    use MVVM pattern to structure the app

--- app/src/main/java/com/laioffer/tinnews/ui/home/HomeFragment.java
+++ app/src/main/java/com/laioffer/tinnews/ui/home/HomeFragment.java
@@ -2,15 +2,22 @@ package com.laioffer.tinnews.ui.home;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.repository.NewsRepository;
+import com.laioffer.tinnews.repository.NewsViewModelFactory;
 
 public class HomeFragment extends Fragment {
+    private HomeViewModel viewModel;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -26,5 +33,25 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NewsRepository repository = new NewsRepository(getContext());
+        viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository))
+                .get(HomeViewModel.class);
+        viewModel.setCountryInput("us");
+        viewModel
+                .getTopHeadlines()
+                .observe(
+                        getViewLifecycleOwner(),
+                        newsResponse -> {
+                            if (newsResponse != null) {
+                                Log.d("HomeFragment", newsResponse.toString());
+                            }
+                        });
+
     }
 }
--- app/src/main/java/com/laioffer/tinnews/ui/search/SearchFragment.java
+++ app/src/main/java/com/laioffer/tinnews/ui/search/SearchFragment.java
@@ -2,15 +2,22 @@ package com.laioffer.tinnews.ui.search;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.repository.NewsRepository;
+import com.laioffer.tinnews.repository.NewsViewModelFactory;
 
 public class SearchFragment extends Fragment {
+    private SearchViewModel viewModel;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -26,5 +33,24 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NewsRepository repository = new NewsRepository(getContext());
+        viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository))
+                .get(SearchViewModel.class);
+        viewModel.setSearchInput("Covid-19");
+        viewModel
+                .searchNews()
+                .observe(
+                        getViewLifecycleOwner(),
+                        newsResponse -> {
+                            if (newsResponse != null) {
+                                Log.d("SearchFragment", newsResponse.toString());
+                            }
+                        });
     }
 }
