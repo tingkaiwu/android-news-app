208cde9c657ca597ee368493bc1fb9bc33051d7b
Ting Kai Wu <tingkaiwuu@gmail.com>
2020-06-12 00:05:08 -0400

    implement home page

--- app/src/main/java/com/laioffer/tinnews/ui/home/HomeFragment.java
+++ app/src/main/java/com/laioffer/tinnews/ui/home/HomeFragment.java
@@ -13,11 +13,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.databinding.FragmentHomeBinding;
+import com.laioffer.tinnews.model.Article;
 import com.laioffer.tinnews.repository.NewsRepository;
 import com.laioffer.tinnews.repository.NewsViewModelFactory;
+import com.mindorks.placeholderview.SwipeDecor;
 
 public class HomeFragment extends Fragment {
     private HomeViewModel viewModel;
+    private FragmentHomeBinding binding;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -31,13 +35,24 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding
+                .swipeView
+                .getBuilder()
+                .setDisplayViewCount(3)
+                .setSwipeDecor(
+                        new SwipeDecor()
+                                .setPaddingTop(20)
+                                .setRelativeScale(0.01f));
+
+        binding.rejectBtn.setOnClickListener(v -> binding.swipeView.doSwipe(false));
+        binding.acceptBtn.setOnClickListener(v -> binding.swipeView.doSwipe(true));
 
         NewsRepository repository = new NewsRepository(getContext());
         viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository))
@@ -49,7 +64,10 @@ public class HomeFragment extends Fragment {
                         getViewLifecycleOwner(),
                         newsResponse -> {
                             if (newsResponse != null) {
-                                Log.d("HomeFragment", newsResponse.toString());
+                                for (Article article : newsResponse.articles) {
+                                    TinNewsCard tinNewsCard = new TinNewsCard(article);
+                                    binding.swipeView.addView(tinNewsCard);
+                                }
                             }
                         });
 
